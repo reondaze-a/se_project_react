ad33a3542f6913dafab1007e58958349aa0e7996
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-09-01 14:29:35 -0400

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import "./Profile.css";
-import SideBar from "./SideBar/Sidebar";
+import SideBar from "./SideBar/SideBar";
 import ClothesSection from "./ClothesSection/ClothesSection";
 
 export default function Profile({
