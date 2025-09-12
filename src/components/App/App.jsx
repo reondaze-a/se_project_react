3c19a466cb963d8ba99f9b9c883ceb50096e5786
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-09-12 13:41:56 -0400

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 import { locations, dbUrl, weatherURL } from "../../utils/constants";
 import { useAuth } from "../../contexts/CurrentUserContext"
